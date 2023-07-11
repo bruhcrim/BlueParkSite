88c8f471fc6bd660af7961685ab3da342d20528d
Anthony <122399607+bruhcrim@users.noreply.github.com>
2023-07-11 14:38:47 -0600

    Update login

--- logon/login.php
+++ logon/login.php
@@ -13,6 +13,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve form data
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    header("Location: ../readDB.php");
+}
+
 // Close connection
 $conn->close();
 ?>
