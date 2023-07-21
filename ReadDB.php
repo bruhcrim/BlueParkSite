fb259e47c1c7409a8f9d434488b5c5da5257a09a
Anthony <122399607+bruhcrim@users.noreply.github.com>
2023-07-20 18:32:26 -0600

    Update ReadDB.php

--- ReadDB.php
+++ ReadDB.php
@@ -55,12 +55,7 @@ if ($result = $conn->query($sql)) {
         $row_lot_name = $row["lot_name"];
         $row_IsOccupied = $row["IsOccupied"];
         $row_Cost = $row["Cost"];
-        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
-      
-        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
-      
+
         // Count how many spaces are free
         $availableSpaces -= $row_IsOccupied;
 
